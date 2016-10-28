8535456637fcba690406f7e0a5b834025c88af25
panyuerui <panyuerui@xinruan>
2016-10-28 17:03:49 +0800

    init

---
+++ JASidePanels/JASidePanels.h
@@ -0,0 +1,19 @@
+//
+//  JASidePanels.h
+//  JASidePanels
+//
+//  Created by ray on 16/10/28.
+//  Copyright © 2016年 ray. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JASidePanels.
+FOUNDATION_EXPORT double JASidePanelsVersionNumber;
+
+//! Project version string for JASidePanels.
+FOUNDATION_EXPORT const unsigned char JASidePanelsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JASidePanels/PublicHeader.h>
+
+
